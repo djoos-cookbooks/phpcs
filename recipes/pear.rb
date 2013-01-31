b2911a9ec721d8045af3b72a452be0e8db37bdb2
David Joos <david.joos@gmail.com>
2013-01-31 18:26:12 +0000

    Fixes

--- recipes/pear.rb
+++ recipes/pear.rb
@@ -8,13 +8,19 @@
 include_recipe "php"
 
 #PHP Extension and Application Repository PEAR channel
-php_pear_channel "pear.php.net" do
+pearhub_chan = php_pear_channel "pear.php.net" do
   action :update
 end
 
 #upgrade PEAR
 php_pear "PEAR" do
+	channel pearhub_chan.channel_name
 	action :upgrade
+end
+
+#PHPUnit PEAR channel
+php_pear_channel "pear.phpunit.de" do
+	action :discover
 end
 
 #upgrade phpcs
