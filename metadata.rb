3a61e8892de5abc1e94f8fcb2caabb068b05d998
David Joos <david.joos@gmail.com>
2017-09-06 14:30:04 +0100

    Fix for RuboCop issues
    Oh, RuboCop...

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures phpcs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.3.1'
 
-%w(debian ubuntu redhat centos fedora scientific amazon).each do |os|
+%w[debian ubuntu redhat centos fedora scientific amazon].each do |os|
   supports os
 end
 
