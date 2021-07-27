8aaa5fd0d0016c8dfc01c2aa75308d7b1e6751a2
David Joos <david.joos@gmail.com>
2021-07-27 12:31:55 +0100

    version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'David Joos'
 maintainer_email 'development@davidjoos.com'
 license 'MIT'
 description 'Installs/Configures phpcs'
-version '1.4.0'
+version '1.4.1'
 
 %w(debian ubuntu redhat centos fedora scientific amazon).each do |os|
   supports os
